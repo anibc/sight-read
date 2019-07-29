e51ee0bce25f05c843805336b5185e99ea3106d2
anibc <oni74mail@gmail.com>
2019-07-29 22:53:12 +0600

    added method white to translate any note to white keys

--- sightread/note.py
+++ sightread/note.py
@@ -1,6 +1,7 @@
+""" MIDI note object """
+notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'] 
+
 class Note:
-    """ MIDI note object """
-    notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
     def __init__( self, v ):
         if type(v) == int:
             self.n = v
@@ -16,15 +17,17 @@ class Note:
         self._n = value
     @property
     def s(self):
-        mod = Note.notes[ self.n % 12 ]
+        mod = notes[ self.n % 12 ]
         div = self.n // 12
         return str(div) + mod
     @s.setter
     def s(self, value):
         ind = -2 if value[ -2 ].isalpha() else -1
         first, second = value[ :ind ], value[ ind: ]
-        mod = Note.notes.index( second )
+        mod = notes.index( second )
         self.n = int( first ) * 12 + mod
+    def white( self ):
+        return Note( self.s[:-1] if self.s[ -2 ].isalpha() else self.s )
 
 def main():
     n = Note('5C')
